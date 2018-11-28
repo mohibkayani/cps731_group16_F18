e41eb08272ee6007a3cb056aced6ca33b5d04db2
Mohammad Akhtar <akhtar@Mohammads-MacBook-Pro.local>
2018-11-28 18:45:41 -0500

    added some dependencies

--- deal-finder/src/App.js
+++ deal-finder/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './ButtonAppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <ButtonAppBar></ButtonAppBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
